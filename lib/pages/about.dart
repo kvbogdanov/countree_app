430f4f6de5d8e8cf01492392152a3ca00ed335f7
Konstantin Bogdanov <kosyag@gmail.com>
2022-05-20 16:06:25 +0700

    Архангельск

--- lib/pages/about.dart
+++ lib/pages/about.dart
@@ -2,19 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:countree/model/user.dart';
 
-
 import 'package:countree/widgets/drawer.dart';
 
 class AboutPage extends StatefulWidget {
   static const String route = 'about';
 
   @override
-AboutPageState createState() {
+  AboutPageState createState() {
     return AboutPageState();
   }
 }
 
-class AboutPageState extends State<AboutPage>{
+class AboutPageState extends State<AboutPage> {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   bool signed = false;
   User currentUser;
@@ -22,46 +21,36 @@ class AboutPageState extends State<AboutPage>{
   _getLoggedState() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     final res = (prefs.getBool('logged') ?? false);
-    if(res == true)
-      return await loadCurrentUser();
+    if (res == true) return await loadCurrentUser();
     return res;
-  } 
+  }
 
   @override
   void initState() {
     super.initState();
 
-    _getLoggedState().then((result){
-        setState(() {
-          if(result is User)
-          {
-            currentUser = result;
-            signed = true;
-          }
-          else
-            signed = false;
-        });
-    }); 
+    _getLoggedState().then((result) {
+      setState(() {
+        if (result is User) {
+          currentUser = result;
+          signed = true;
+        } else
+          signed = false;
+      });
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
-      key: _scaffoldKey,
-      appBar: AppBar(title: Text('О проекте')),
-      endDrawer: buildDrawer(context, AboutPage.route, signed:signed, cu: currentUser),
-      body: Padding(
-        padding: EdgeInsets.all(15.0),
-        child: Column(
-          children: [
-            Image.asset('assets/images/mainlogo.png'),
-            Text('Мы – красноярская общественная организация «Живой город». В 2018 году мы создали систему учёта городских зелёных насаждений, с помощью которой волонтеры уже нанесли на карту более 22 тысяч деревьев и кустарников в 3 городах Красноярского края. Опыт других стран показывает, что такие интерактивные карты помогают защищать уже растущие деревья и выбирать подходящие места для новых, эффективно расходуя городской бюджет.'
-            , style: TextStyle(fontSize: 16)),
-          ]
-        )
-      )
-    );
+        key: _scaffoldKey,
+        appBar: AppBar(title: Text('О проекте')),
+        endDrawer: buildDrawer(context, AboutPage.route, signed: signed, cu: currentUser),
+        body: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: Column(children: [
+              Image.asset('assets/images/mainlogo.png'),
+              Text('', style: TextStyle(fontSize: 16)),
+            ])));
   }
-
 }
